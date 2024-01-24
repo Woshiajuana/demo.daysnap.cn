e08c0f7f166c5d32a7cac265118f581255d3cc4d
chenzhigang <979703986@qq.com>
2024-01-24 16:37:13 +0800

    chore: 更新

--- pixijs-stage/src/views/index/index.js
+++ pixijs-stage/src/views/index/index.js
@@ -4,9 +4,11 @@ import './index.scss'
 
 // 初始化
 const app = new PIXI.Application({
+  width: window.innerWidth,
+  height: window.innerHeight,
   antialias: true,
   // backgroundAlpha: false,
-  resolution: 2,
+  resolution: window.devicePixelRatio || 1,
 })
 $('#app').append(app.view)
 
@@ -19,9 +21,23 @@ app.renderer.autoResize = true
 onResize()
 window.addEventListener('resize', onResize)
 
+// 创建一个矩形
+const rectangle = new PIXI.Graphics()
+rectangle.beginFill(0x66ccff) // 填充颜色
+rectangle.drawRect(0, 0, 164, 64)
+rectangle.endFill()
+
+rectangle.interactive = true
+rectangle.on('click', () => {
+  console.log('rectangle')
+})
+
+// 将矩形添加到舞台
+app.stage.addChild(rectangle)
+
 // 加载图片
-const texture = await PIXI.Assets.load('assets/img/bg0.jpeg')
-const bunny = new PIXI.Sprite(texture)
+// const texture = await PIXI.Assets.load('assets/img/bg0.jpeg')
+// const bunny = new PIXI.Sprite(texture)
 
 // // Setup the position of the bunny
 // bunny.x = app.renderer.width / 2
@@ -39,3 +55,51 @@ const bunny = new PIXI.Sprite(texture)
 //   // each frame we spin the bunny around a bit
 //   // bunny.rotation += 0.01
 // })
+
+/**
+ * 快速排序
+ */
+
+// 快速排序
+
+// 快速排序
+
+/** 快速排序 */
+
+// 快速排序
+
+/** 快速排序 */
+
+//快速排序
+
+// 快速排序
+
+// 冒泡排序
+
+//快速排序
+
+// 快速排序
+
+//快速排序
+
+//冒泡排序
+
+// 计算两个数想加
+function add(x, y) {
+  return x + y
+}
+
+// 计算两个数相减
+function minus(x, y) {
+  return x - y
+}
+
+// 计算两个数相乘
+function multiply(x, y) {
+  return x * y
+}
+
+// 计算两个数相除
+function divide(x, y) {
+  return x / y
+}
