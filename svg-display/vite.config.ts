eb53d249abd5f9dd75cb11dee688f9f129e584a0
chenzhigang <979703986@qq.com>
2024-07-01 13:48:50 +0800

    chore: svg demo

---
+++ svg-display/vite.config.ts
@@ -0,0 +1,45 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import postCssPxToRem from 'postcss-pxtorem'
+import AutoImport from 'unplugin-auto-import/vite'
+import { VantResolver } from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+
+    // https://github.com/antfu/unplugin-auto-import
+    AutoImport({
+      imports: ['vue', 'vue-router'],
+      dts: 'typings/auto-imports.d.ts',
+      resolvers: [VantResolver()],
+    }),
+
+    Components({
+      resolvers: [VantResolver()],
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  css: {
+    devSourcemap: true,
+    postcss: {
+      plugins: [
+        // https://www.npmjs.com/package/postcss-pxtorem
+        postCssPxToRem({
+          rootValue: 37.5, // 1rem的大小
+          propList: ['*'], // 需要转换的属性，这里选择全部都进行转换
+        }),
+      ],
+    },
+  },
+})
