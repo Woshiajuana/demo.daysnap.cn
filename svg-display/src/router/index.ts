4448c209acda1299568d15cbc7b4622980a2b9a7
chenzhigang <979703986@qq.com>
2024-07-01 13:58:59 +0800

    feat: init project

--- svg-display/src/router/index.ts
+++ svg-display/src/router/index.ts
@@ -1,7 +1,7 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+export const router = createRouter({
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
@@ -10,5 +10,3 @@ const router = createRouter({
     },
   ],
 })
-
-export default router
