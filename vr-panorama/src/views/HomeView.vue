89772e16a8eee85f9916efe31441c6676372e8b9
chenzhigang <979703986@qq.com>
2024-07-11 09:01:17 +0800

    feat: sphere

--- vr-panorama/src/views/HomeView.vue
+++ vr-panorama/src/views/HomeView.vue
@@ -1,9 +1,18 @@
 <template>
-  <main class="home-wrap">
-  </main>
+  <div id="viewer" style="width: 100vw; height: 100vh"></div>
 </template>
 
 <script setup lang="ts">
+  import { Viewer } from '@photo-sphere-viewer/core'
+
+  onMounted(() => {
+    const viewer = new Viewer({
+      container: 'viewer',
+      panorama: '/img/sphere.jpeg',
+      loadingImg: '/img/loader.gif',
+      caption: 'Parc national du Mercantour <b>&copy; Damien Sorel</b>',
+    })
+  })
 </script>
 
 <style lang="scss" scoped>
