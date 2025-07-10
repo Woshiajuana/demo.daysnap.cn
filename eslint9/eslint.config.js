5c9888a0e5a6bbfcd1ead49aa9d92fed4115c35b
chenzhigang <979703986@qq.com>
2025-07-10 16:46:47 +0800

    feat: 3d

--- eslint9/eslint.config.js
+++ eslint9/eslint.config.js
@@ -3,12 +3,11 @@ import globals from "globals";
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-console.log('111', tseslint.configs.recommended)
-
 export default defineConfig([
-  { ignores: ['**/node_modules/**', '**/*.config.js'] },
-  { files: ['src/**/*.{js,ts}']},
+  { ignores: [] },
+  // { ignores: ['**/node_modules/**', '**/*.config.js'] },
+  { name: '全部', files: ['src/**/*.{js,ts}'] },
   // { files: ["**/*.{js,mjs,cjs,ts,vue}"], languageOptions: { globals: globals.browser } },
-  js.configs.recommended,
-  tseslint.configs.recommended,
+  // js.configs.recommended,
+  // tseslint.configs.recommended,
 ]);
